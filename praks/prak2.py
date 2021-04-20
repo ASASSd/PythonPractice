d13ca11d104581109c1f92daf7157d81d8bd633a
Ilya <ilyasmirkin200137@gmail.com>
2021-04-20 22:53:52 +0300

    f23 made

--- praks/prak2.py
+++ praks/prak2.py
@@ -56,3 +56,18 @@ def f22(x):
 #print(hex(f2(0x8f6903fd)))
 
 
+def f23(x):
+    outx = []
+    j=0
+    for i in range(len(x)):
+        if x[i] != [None, None]:
+            x[i][1] = x[i][1].split('#')
+            x[i][0] = str(round(float(x[i][0]), 1))
+            outx.append([str(x[i][0]), x[i][1][1], x[i][1][0]])
+            outx[j][1] = outx[j][1].replace('-', '/')
+            outx[j][2] = outx[j][2][6:16]
+            j += 1
+    return outx
+
+
+#print(f23([['0.81', '(375) 147-82-60#2003-02-27'], ['0.47', '(932) 370-41-56#2004-02-21'], [None, None], ['0.57', '(453) 328‐83‐11#2001‐03‐03']]))
