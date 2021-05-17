4d28fa78d7adeb2a8465d6f884ad1de91485e9cb
Ilya <ilyasmirkin200137@gmail.com>
2021-05-17 11:11:06 +0000

    f23 done mfk

--- praks/prak2.py
+++ praks/prak2.py
@@ -64,8 +64,8 @@ def f23(x):
             x[i][1] = x[i][1].split('#')
             x[i][0] = str(round(float(x[i][0]), 1))
             outx.append([str(x[i][0]), x[i][1][1], x[i][1][0]])
-            date = outx[j][1].split('‐')
-            date_str = date[-2] + '/' + date[-1] + '/' + date[0]
+            date = outx[j][1].split('-')
+            date_str = date[2] + '/' + date[1] + '/' + date[0]
             outx[j][1] = date_str
             outx[j][2] = outx[j][2][6:16]
             j += 1
